4fd112b7a8f200562153b64924d63c3581249843
BinodKarki <karki.binod919@gmail.com>
2020-01-22 12:29:54 -0700

    String concatenation implemented

--- Homework_01/app/src/main/java/com/homework_01/MainActivity.kt
+++ Homework_01/app/src/main/java/com/homework_01/MainActivity.kt
@@ -2,11 +2,18 @@ package com.homework_01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        concat_button.setOnClickListener {
+            concat_view.text = "Concatenated Strings\n ${input_text_1.text}" + " ${input_text_2.text}"
+        }
+
     }
 }
