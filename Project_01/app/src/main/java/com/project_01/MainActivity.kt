4261cb8e2f065fde69030f987477f2a2a405e89a
BinodKarki <karki.binod919@gmail.com>
2020-02-17 22:58:27 -0700

    trying out activities with intent

--- Project_01/app/src/main/java/com/project_01/MainActivity.kt
+++ Project_01/app/src/main/java/com/project_01/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.project_01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // when this button is clicked, new activity should launch
+        click_button.setOnClickListener {
+
+            //set intent to launch second activity
+            val intent: Intent = Intent(this, SecondActivity::class.java)
+            startActivityForResult(intent, 10)                              //launch intent with request code
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        
     }
 }
---
+++ Project_01/app/src/main/java/com/project_01/SecondActivity.kt
@@ -0,0 +1,19 @@
+package com.project_01
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        display.setText("Second Activity")
+
+    }
+
+
+}
