64644e8a733952a1af3878f76a2e811e4bff2700
BinodKarki <binodkarki@u.boisestate.edu>
2020-05-06 12:00:22 -0600

    Final Project

--- FinalProject/app/src/main/java/com/e/finalproject/MainActivity.kt
+++ FinalProject/app/src/main/java/com/e/finalproject/MainActivity.kt
@@ -3,19 +3,42 @@ package com.e.finalproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var alarmStatus: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(alarmStatus != null){
+            alm_stat.visibility = View.VISIBLE
+            alm_stat_disp.visibility = View.VISIBLE
+            alm_stat_disp.setText(alarmStatus)
+        }
 
         btn_set_alarm.setOnClickListener {
 
             val intent: Intent = Intent(this, SecondActivity::class.java)
             startActivityForResult(intent, 10)
 
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(data != null){
+            alarmStatus = data.getStringExtra("AlarmSet")
+            if(alarmStatus != null) {
+                alm_stat.visibility = View.VISIBLE
+                alm_stat_disp.visibility = View.VISIBLE
+                alm_stat_disp.setText(alarmStatus)
+            }
         }
 
     }
--- FinalProject/app/src/main/java/com/e/finalproject/SecondActivity.kt
+++ FinalProject/app/src/main/java/com/e/finalproject/SecondActivity.kt
@@ -1,8 +1,11 @@
 package com.e.finalproject
 
+import android.app.Activity
+import android.content.Intent
 import android.media.Ringtone
 import android.media.RingtoneManager
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_second.*
@@ -12,7 +15,7 @@ import kotlin.concurrent.timerTask
 
 class SecondActivity : AppCompatActivity() {
 
-    lateinit var alarmSetTime: String
+     var alarmSetTime: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,39 +23,63 @@ class SecondActivity : AppCompatActivity() {
 
         val timer: Timer = Timer();
 
+        val ringtone: Ringtone = RingtoneManager.getRingtone(
+            applicationContext,
+            RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+        )
 
         btn_set.setOnClickListener {
 
             val hr = time_picker.hour
             val mi = time_picker.minute
 
-            val ringtone: Ringtone = RingtoneManager.getRingtone(
-                applicationContext,
-                RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
-            )
             timer.scheduleAtFixedRate(timerTask {
 
-                var tcurrentTime = txt_clock.text.toString()
-
-                var currentTime = tcurrentTime.replaceRange(5, 8, "")
+                var currentTime = txt_clock.text.toString()
+                currentTime = currentTime.replaceRange(5, 8, "")
 
                 if (currentTime.equals(alarmTime())) {
                     ringtone.play()
-                    val i = 0
                 } else {
                     ringtone.stop()
                 }
 
             }, 0, 1000)
+
+                alarmSetTime = alarmTime()
+
+                tv_setTo.visibility = View.VISIBLE
+                tv_alm_disp.visibility = View.VISIBLE
+
+                tv_setTo.text = "Alarm Set To: "
+                tv_alm_disp.text = alarmSetTime
+
         }
+
     }
 
+    //overide back button for return intent
+    override fun onBackPressed() {
+
+        var returnIntent: Intent = Intent()
+        returnIntent.putExtra("AlarmSet",alarmSetTime)
+        setResult(Activity.RESULT_OK, returnIntent)
+        super.onBackPressed()
+    }
+
+
+
+    /**
+     * A private helper function to get the alarm time selected
+     * by the time picker.
+     *
+     * @return returns a string representation of time in 'HH:MM AM/PM' format
+     */
     private fun alarmTime(): String {
 
-        var alHours = time_picker.hour
-        //val alMin = time_picker.minute
         val alMin:String;
         var alTime: String;
+        var alHours = time_picker.hour
 
         if(time_picker.minute < 10){
             alMin = "0" + time_picker.minute.toString()
@@ -68,12 +95,16 @@ class SecondActivity : AppCompatActivity() {
                 alTime = ""
             alTime += alHours.toString() + ":" + alMin + " PM"
         } else {
-            alTime = alHours.toString() + ":" + alMin + " AM"
+            if(alHours < 10){
+                alTime = "0"
+            }else{
+                alTime = ""
+            }
+            alTime += alHours.toString() + ":" + alMin + " AM"
         }
 
-        alarmSetTime = alTime
+        //alarmSetTime = alTime
         return alTime
-
     }
 
 }
